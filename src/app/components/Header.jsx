bbf90e572331da2967911badfeeb28d67a032478
ahsan.habib01 <ahshapon00@gmail.com>
2025-12-13 00:37:42 +0600

    initialized the project

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className='px-3 py-4 border-b-2 border-gray-400'>
+      <h1 className='text-lg'>👨‍💻 Dev-Story</h1>
+    </header>
+  );
+};
+
+export default Header;
