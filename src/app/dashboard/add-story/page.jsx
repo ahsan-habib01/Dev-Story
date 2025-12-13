53a31b4b949b5cff330f5d371b2499e5f9555411
ahsan.habib01 <ahshapon00@gmail.com>
2025-12-13 11:13:31 +0600

    create dashboard layout

---
+++ src/app/dashboard/add-story/page.jsx
@@ -0,0 +1,10 @@
+import Title from '@/components/Title'
+import React from 'react'
+
+export default function addStory() {
+  return (
+    <div>
+      <Title>Add Your Story</Title>
+    </div>
+  )
+}
